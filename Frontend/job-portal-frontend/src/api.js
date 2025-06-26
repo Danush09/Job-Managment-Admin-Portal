50da3e660655d4efb7369f1165cee0415472278f
Danush09 <136923185+Danush09@users.noreply.github.com>
2025-06-26 13:26:29 +0530

    Update api.js

--- Frontend/job-portal-frontend/src/api.js
+++ Frontend/job-portal-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://job-managment-admin-portal.vercel.app/',
+    baseURL: 'https://job-managment-admin-portal-backend.onrender.com',
 });
 
 export default API; 
